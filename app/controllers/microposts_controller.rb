b47ccc9aa92fa2fa75cde172d8b75e3af0ae9a3c
leetenki <toukyouniversityoftechnology@gmail.com>
2015-08-19 02:53:24 +0900

    add posting micropost

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,14 +1,25 @@
 class MicropostsController < ApplicationController
-	before_action :logged_in_user, :only => [:create]
+	before_action :logged_in_user, :only => [:create, :destroy]
 
 	def create
-		@micropost = current_user.microposts.build(microposts_params)
+		@micropost = current_user.microposts.build(micropost_params)
 		if @micropost.save
 			flash[:success] = "Micropost created!"
 			redirect_to root_url
 		else
 			render 'static_pages/home'
 		end
+	end
+
+	def destroy
+		@micropost = current_user.microposts.find_by(:id => params[:id])
+		if @micropost.nil?
+			redirect_to root_url
+		end
+
+		@micropost.destroy
+		flash[:success] = "Succeed to destroy micropost"
+		redirect_to request.referrer || root_url
 	end
 
 	private
