f47da746026fc8ebb6c805865149cca887e96171
leetenki <toukyouniversityoftechnology@gmail.com>
2015-08-20 07:45:48 +0900

    add home view's timeline

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,69 +1,69 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, :only => [:edit, :update, :followings, :followers, :tweets]
 
-	#render following page
-	def followings
-		@following_users = User.find(params[:id]).following_users
-	end
+  #render following page
+  def followings
+    @following_users = User.find(params[:id]).following_users
+  end
 
-	#render followers page
-	def followers
-		@follower_users = User.find(params[:id]).follower_users
-	end
+  #render followers page
+  def followers
+    @follower_users = User.find(params[:id]).follower_users
+  end
 
-	#render tweets page
-	def tweets
-  	@user = User.find(params[:id])
-  	@microposts = @user.microposts
-	end
+  #render tweets page
+  def tweets
+    @user = User.find(params[:id])
+    @microposts = @user.microposts
+  end
 
 
   #signup page
   def new
-  	@user = User.new
+    @user = User.new
   end
 
   #save to database
   def create
-  	@user = User.new(user_params)
-  	if @user.save
-  		flash[:success] = "Welcome to the micropost App!"
-  		redirect_to user_path(@user)
-  	else
-  		render 'new'
-  	end
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the micropost App!"
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
   end
 
   #edit
   def edit
-  	@user = current_user
+    @user = current_user
   end
 
   #update
   def update
-  	if @current_user.update(user_params)
-  		flash[:success] = "Succeed to update profile!"
-  		redirect_to user_path(@current_user)
-		else
-			@user = current_user
-	    render 'edit'
-		end
+    if current_user.update(user_params)
+      flash[:success] = "Succeed to update profile!"
+      redirect_to user_path(@current_user)
+    else
+      @user = current_user
+      render 'edit'
+    end
   end
 
   #subregion
   def subregion_options
-  	render :partial => 'subregion_select'
+    render :partial => 'subregion_select'
   end
 
   #show
   def show
-  	@user = User.find(params[:id])
-  	@microposts = @user.microposts
+    @user = User.find(params[:id])
+    @microposts = @user.microposts
   end
 
   #avoid xss
   private
   def user_params
-  	params.require(:user).permit(:name, :email, :country_code, :state_code, :profile, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :country_code, :state_code, :profile, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,52 +1,56 @@
 class User < ActiveRecord::Base
-	has_many :microposts
-	has_many :following_relationships, :class_name => "Relationship", :foreign_key => :follower_id, :dependent => :destroy
-	has_many :following_users, :through => :following_relationships, :source => :followed
-	has_many :follower_relationships, :class_name => "Relationship", :foreign_key => :followed_id, :dependent => :destroy
-	has_many :follower_users, :through => :follower_relationships, :source => :follower
+  has_many :microposts
+  has_many :following_relationships, :class_name => "Relationship", :foreign_key => :follower_id, :dependent => :destroy
+  has_many :following_users, :through => :following_relationships, :source => :followed
+  has_many :follower_relationships, :class_name => "Relationship", :foreign_key => :followed_id, :dependent => :destroy
+  has_many :follower_users, :through => :follower_relationships, :source => :follower
 
-	before_save { self.email = email.downcase }
+  before_save { self.email = email.downcase }
 
-	validates :name, :presence => true, :length => { :maximum => 50 }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, 
-			  :presence => true, 
-			  :length => { :maximum => 255 }, 
-			  :format => { :with => VALID_EMAIL_REGEX },
-			  :uniqueness => { :case_sensitive => false }
-	validates :country_code, :presence => true, :inclusion => { :in => Carmen::Country.all.map(&:code) }
-	validates :profile, :length => { :maximum => 150 }
-	validate :check_state_code
-	has_secure_password
+  validates :name, :presence => true, :length => { :maximum => 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, 
+        :presence => true, 
+        :length => { :maximum => 255 }, 
+        :format => { :with => VALID_EMAIL_REGEX },
+        :uniqueness => { :case_sensitive => false }
+  validates :country_code, :presence => true, :inclusion => { :in => Carmen::Country.all.map(&:code) }
+  validates :profile, :length => { :maximum => 150 }
+  validate :check_state_code
+  has_secure_password
 
+  #get time line
+  def feed_items
+    Micropost.where(:user_id => following_user_ids + [self.id])
+  end
 
-	#to follow other user
-	def follow(other_user)
-		following_relationships.create(followed_id: other_user.id)
-	end
+  #to follow other user
+  def follow(other_user)
+    following_relationships.create(followed_id: other_user.id)
+  end
 
-	#to unfollow other user
-	def unfollow(other_user)
-		following_relationships.find_by(followed_id: other_user.id).destroy
-	end
+  #to unfollow other user
+  def unfollow(other_user)
+    following_relationships.find_by(followed_id: other_user.id).destroy
+  end
 
-	#is following
-	def following?(other_user)
-		following_users.include?(other_user)
-	end
+  #is following
+  def following?(other_user)
+    following_users.include?(other_user)
+  end
 
-	private
-	def check_state_code
-		if country_code.in? Carmen::Country.all.map(&:code)
-			subregions = Carmen::Country.coded(country_code).subregions.map(&:code)
-			if subregions.empty?
-				if state_code.length > 60
-					errors.add(:state_code, 'is too long!')
-				end
-			elsif (state_code.in? subregions)
-			else
-				errors.add(:state_code, 'is incorrect value!')
-			end
-		end
-	end
+  private
+  def check_state_code
+    if country_code.in? Carmen::Country.all.map(&:code)
+      subregions = Carmen::Country.coded(country_code).subregions.map(&:code)
+      if subregions.empty?
+        if state_code.length > 60
+          errors.add(:state_code, 'is too long!')
+        end
+      elsif (state_code.in? subregions)
+      else
+        errors.add(:state_code, 'is incorrect value!')
+      end
+    end
+  end
 end
